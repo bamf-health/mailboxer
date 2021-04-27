f397136a2710405b36682ad2a2499bb0980c357e
Trannie Carter <trannie.carter@bamfhealth.com>
2021-04-27 00:53:56 -0400

    Add magic to get ActiveRecord::Migration loaded

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+puts ActiveRecord::Migration.inspect
+
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 require "mailboxer"
 
