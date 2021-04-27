4c7d85ba820158414a53b5130b2a3727630a4b07
Trannie Carter <trannie.carter@bamfhealth.com>
2021-04-27 00:48:22 -0400

    Fix attachment test for activestorage backend

--- spec/models/mailboxer_models_messageable_spec.rb
+++ spec/models/mailboxer_models_messageable_spec.rb
@@ -308,9 +308,9 @@ describe "Mailboxer::Models::Messageable through User" do
   end
 
   it "should be able to read attachment" do
-    @receipt = @entity1.send_message(@entity2, "Body", "Subject", nil, File.open('spec/testfile.txt'))
+    @receipt = @entity1.send_message(@entity2, "Body", "Subject", nil, [ { io: File.open('spec/testfile.txt'), filename: 'testfile.txt' } ])
     @conversation = @receipt.conversation
-    expect(@conversation.messages.first.attachment_identifier).to eq 'testfile.txt'
+    expect(@conversation.messages.first.attachments.first.filename).to eq 'testfile.txt'
   end
 
   it "should be the same message time as passed" do
